18a07daef82f4f9959a146acf42be69902ee4a48
teerawat prarom <teewat@yahoo.com>
2021-09-12 23:17:35 +1000

    completed assignment

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+const handleClick = () => {
+
+}
+const SimplerComponent = (props) => {
+    return <div onClick={props.handleClick}>I am just happy</div>
+}
+
+export default SimplerComponent;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => { alert('aaa')}} />
   </div>,
   document.getElementById('root')
 );
